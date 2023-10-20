ece6bf702a9382e57a7172fb5b6edd63bf0cd1d3
yuba <Kryuel17@gmail.com>
2023-10-20 12:02:16 -0700

    printf

--- printf_int.c
+++ printf_int.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdarg.h>
+
 /**
  * print_int - Print an integer.
  * @args: Argument list containing the integer
@@ -7,34 +7,35 @@
  */
 int print_int(va_list args)
 {
-    int n = va_arg(args, int);
-    int count = 0;
+	int n = va_arg(args, int);
+	int count = 0;
 
-    int exp = 1;
-    int num = n;
+	int exp = 1;
+	int num = n;
 
-    if (n < 0)
-    {
-        _putchar('-');
-        count++;
-        n = -n;
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		n = -n;
+	}
 
-    while (num > 9)
-    {
-        num /= 10;
-        exp *= 10;
-    }
-    do {
-        int digit = n / exp;
+	while (num > 9)
+	{
+		num /= 10;
+		exp *= 10;
+	}
 
-        _putchar(digit + '0');
-        n %= exp;
-        exp /= 10;
-        count++;
-    } while (exp > 0);
+	do {
+		int digit = n / exp;
 
-    return count;
+		_putchar(digit + '0');
+		n %= exp;
+		exp /= 10;
+		count++;
+	} while (exp > 0);
+
+	return (count);
 }
 
 /**
@@ -44,40 +45,41 @@ int print_int(va_list args)
  */
 int print_dec(va_list args)
 {
-    int n = va_arg(args, int);
-    int i = 0;
+	int n = va_arg(args, int);
+	int i = 0;
 
-    int exp = 1;
-    int num = n;
+	int exp = 1;
+	int num = n;
 
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-        i++;
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+		i++;
+	}
 
-    if (n == 0)
-    {
-        _putchar('0');
-        return 1;
-    }
+	if (n == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
 
-    while (num > 9)
-    {
-        num /= 10;
-        exp *= 10;
-    }
+	while (num > 9)
+	{
+		num /= 10;
+		exp *= 10;
+	}
 
-    while (exp > 0)
-    {
-        int digit = n / exp;
+	while (exp > 0)
+	{
+		int digit = n / exp;
 
-        _putchar(digit + '0');
-        n %= exp;
-        exp /= 10;
-        i++;
-    }
+		_putchar(digit + '0');
+		n %= exp;
+		exp /= 10;
+		i++;
+	}
 
-    return i;
+	return (i);
 }
+
