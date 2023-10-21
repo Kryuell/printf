40c25f50f4b19e73033438c77fc20ee96e2b3793
yuba <Kryuel17@gmail.com>
2023-10-21 06:24:40 -0700

    fix

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _printf - Produces output according to a format.
  * @format: A character string containing format specifiers.
@@ -6,55 +7,45 @@
  */
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list args;
-    int spec_count = 0; /* Declare spec_count at the beginning */
+	int count = 0;
+	va_list args;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            
-            switch (*format)
-            {
-                case 'c':
-                    spec_count = print_c(args);
-                    break;
-                case 's':
-                    spec_count = print_s(args);
-                    break;
-                case 'd':
-                case 'i':
-                    spec_count = print_d(args);
-                    break;
-                case '%':
-                    spec_count = print_percent(args);
-                    break;
-                default:
-                    _putchar('%');
-                    if (*format)
-                    {
-                        _putchar(*format);
-                        spec_count = 2;
-                    }
-                    else
-                        spec_count = 1;
-                    break;
-            }
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+				_putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char*);
+				for (; *str; str++)
+				{
+					_putchar(*str);
+					count++;
+				}
+			}
+			else if (*format == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
 
-            count += spec_count;
-        }
-        else
-        {
-            _putchar(*format);
-            count++;
-        }
-        format++;
-    }
-
-    va_end(args);
-    return count;
+	return (count);
 }
--- helpers.c
+++ helpers.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "helpers.h"
 #include <unistd.h>
 
 /**
